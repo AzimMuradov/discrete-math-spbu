02ee2c495bd15ef0d5c858b868521dbafe2fe626
Azim Muradov <azim.muradov.dev@gmail.com>
2022-03-26 15:47:46 +0300

    Add `ArithmeticCompressor`

--- task-1/lib/src/main/kotlin/compressor/Compressor.kt
+++ task-1/lib/src/main/kotlin/compressor/Compressor.kt
@@ -1,6 +1,12 @@
 package compressor
 
-public interface Compressor<I : Iterable<Byte>> {
+public interface Compressor<in In : Iterable<T>, T, out Out : Iterable<Byte>, M> {
 
-    public fun I.compress(): I
+    public fun compress(message: In): CompressedMessage<Out, M>
 }
+
+
+public data class CompressedMessage<out I : Iterable<Byte>, M>(
+    val compressed: I,
+    val metadata: M,
+)
---
+++ task-1/lib/src/main/kotlin/compressor/compressors/ArithmeticCompressor.kt
@@ -0,0 +1,89 @@
+package compressor.compressors
+
+import compressor.*
+import compressor.compressors.ArithmeticCompressor.Metadata
+import java.math.*
+import kotlin.math.ceil
+import kotlin.math.log2
+
+public class ArithmeticCompressor<I : Collection<T>, T : Comparable<T>> : Compressor<I, T, Collection<Byte>, Metadata> {
+
+    override fun compress(message: I): CompressedMessage<Collection<Byte>, Metadata> {
+        val (_, messageLength) = message.toMessageInfo()
+        val codedSegment = getIntegerSegmentCode(message)
+        val bits = getBinCode(messageLength, codedSegment)
+
+        return CompressedMessage(
+            compressed = Code(bits).toByteList(),
+            metadata = Metadata(messageLength, compressedBitsLength = bits.size)
+        )
+    }
+
+
+    private fun getIntegerSegmentCode(message: I): Segment<BigInteger> {
+        val (countedSymbols, messageLength) = message.toMessageInfo()
+
+        val (symbols, counts) = countedSymbols.toSortedMap().toList().unzip()
+        val bigIntegerCounts = counts.map(Int::toBigInteger)
+        val normalizer = messageLength.toBigInteger()
+
+        fun Segment<BigInteger>.segments(): List<Segment<BigInteger>> {
+            val unit = r - l
+            return bigIntegerCounts
+                .runningFold(initial = BigInteger.ZERO) { acc, c -> acc + c }
+                .zipWithNext()
+                .map { (lI, rI) ->
+                    Segment(l * normalizer + lI * unit, l * normalizer + rI * unit)
+                }
+        }
+
+        return message.fold(initial = Segment(l = BigInteger.ZERO, r = BigInteger.ONE)) { acc, s ->
+            acc.segments()[symbols.indexOf(s)]
+        }
+    }
+
+    private fun getBinCode(messageLength: Int, integerCodeSegment: Segment<BigInteger>): List<Bit> {
+        val scale = messageLength * ceil(log2(messageLength.toDouble())).toInt()
+
+        fun BigInteger.downscale(): BigDecimal = toBigDecimal().divide(
+            messageLength.toBigDecimal().pow(messageLength),
+            scale,
+            RoundingMode.HALF_EVEN
+        )
+
+        val codeSegment = with(integerCodeSegment) { Segment(l = l.downscale(), r = r.downscale()) }
+
+        fun Segment<BigDecimal>.middle() = (l + r).divide(2.toBigDecimal(), scale, RoundingMode.HALF_EVEN)
+
+        val binaryCodeSegment = generateSequence(seed = Segment(BigDecimal.ZERO, BigDecimal.ONE)) { seg ->
+            if (seg.middle() !in codeSegment) {
+                val m = seg.middle()
+                val (leftSeg, rightSeg) = Segment(seg.l, m) to Segment(m, seg.r)
+
+                if (codeSegment.r in leftSeg) leftSeg else rightSeg
+            } else {
+                null
+            }
+        }.last()
+
+        return buildList {
+            var middle = binaryCodeSegment.middle().stripTrailingZeros()
+            while (middle != BigDecimal.ZERO) {
+                val twice = (middle * 2.toBigDecimal()).stripTrailingZeros()
+                add(twice.toInt().toBit())
+                middle = twice % BigDecimal.ONE
+            }
+        }
+    }
+
+
+    public data class Metadata(
+        val messageLength: Int,
+        val compressedBitsLength: Int,
+    )
+
+
+    private data class Segment<T : Comparable<T>>(val l: T, val r: T)
+
+    private operator fun <T : Comparable<T>> Segment<T>.contains(element: T) = element in l..r
+}
